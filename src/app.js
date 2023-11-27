14bad2780296849c284016d0b444f64653454c5d
VincentRssx <Vincent.rssx59@gmail.com>
2023-11-27 16:55:00 +0100

    switch

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
+require("dotenv").config();
 const express = require("express");
+
+const port = process.env.APP_PORT;
 
 const app = express();
 
@@ -6,5 +9,8 @@ const movieControllers = require("./controllers/movieControllers");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+app.get("/api/users", movieControllers.getAllUsers);
+app.get("/api/users/:id", movieControllers.getUsersById);
+app.post("/api/movies", movieControllers.postMovie);
 
 module.exports = app;
