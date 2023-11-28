4b1341aa3b21002d7f720404524f7ad104adede5
VincentRssx <Vincent.rssx59@gmail.com>
2023-11-28 16:34:53 +0100

    test ok

--- tests/users.test.js
+++ tests/users.test.js
@@ -66,3 +66,76 @@ describe("POST /api/users", () => {
 		expect(usersInDatabase.title).toStrictEqual(newUser.title);
 	});
 });
+
+describe("PUT /api/users/:id", () => {
+	it("should edit movie", async () => {
+		const newUser = {
+			firstname: "Maria",
+			lastname: "Martine",
+			email: `${crypto.randomUUID()}@wild.co`,
+			city: "Lille",
+			language: "French",
+		};
+
+		const [result] = await database.query(
+			"INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+			[
+				newUser.firstname,
+				newUser.lastname,
+				newUser.email,
+				newUser.city,
+				newUser.language,
+			]
+		);
+
+		const id = result.insertId;
+
+		const updatedUsers = {
+			firstname: "Maria",
+			lastname: "Martine",
+			email: `${crypto.randomUUID()}@wild.co`,
+			city: "Lille",
+			language: "French",
+		};
+
+		const response = await request(app)
+			.put(`/api/users/${id}`)
+			.send(updatedUsers);
+
+		expect(response.status).toEqual(204);
+
+		const [users] = await database.query(
+			"SELECT * FROM users WHERE id=?",
+			id
+		);
+
+		const [usersInDatabase] = users;
+
+		expect(usersInDatabase).toHaveProperty("id");
+
+		expect(usersInDatabase).toHaveProperty("firstname");
+		expect(usersInDatabase.firstname).toStrictEqual(updatedUsers.firstname);
+
+		expect(usersInDatabase).toHaveProperty("lastname");
+		expect(usersInDatabase.lastname).toStrictEqual(updatedUsers.lastname);
+
+		expect(usersInDatabase).toHaveProperty("email");
+		expect(usersInDatabase.email).toStrictEqual(updatedUsers.email);
+
+		expect(usersInDatabase).toHaveProperty("city");
+		expect(usersInDatabase.city).toStrictEqual(updatedUsers.city);
+
+		expect(usersInDatabase).toHaveProperty("language");
+		expect(usersInDatabase.language).toStrictEqual(updatedUsers.language);
+	});
+
+	it("should return an error", async () => {
+		const usersWithMissingProps = { title: "Harry Potter" };
+
+		const response = await request(app)
+			.put(`/api/users/1`)
+			.send(usersWithMissingProps);
+
+		expect(response.status).toEqual(500);
+	});
+});
